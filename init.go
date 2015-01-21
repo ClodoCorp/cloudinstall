845f11a7acad5b282f59d197680dae674647ed71
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-01-21 16:00:08 +0300

    some checks
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- init.go
+++ init.go
@@ -14,9 +14,12 @@ var (
 
 func init() {
 	var err error
-	err = os.Mkdir("/proc", 0755)
-	if err != nil {
-		fmt.Printf("failed to create /proc: %s\n", err.Error())
+
+	if _, err := os.Stat("/proc"); err != nil {
+		err = os.Mkdir("/proc", 0755)
+		if err != nil {
+			fmt.Printf("failed to create /proc: %s\n", err.Error())
+		}
 	}
 	err = syscall.Mount("proc", "/proc", "proc", 0, "")
 	if err != nil {
@@ -31,15 +34,18 @@ func init() {
 	}
 	cmdline = strings.Split(strings.TrimSpace(string(buf)), " ")
 
-	err = os.Mkdir("/sys", 0755)
-	if err != nil {
-		fmt.Printf("failed to create /sys: %s\n", err.Error())
+	if _, err := os.Stat("/sys"); err != nil {
+		err = os.Mkdir("/sys", 0755)
+		if err != nil {
+			fmt.Printf("failed to create /sys: %s\n", err.Error())
+		}
 	}
 	err = syscall.Mount("sys", "/sys", "sysfs", 0, "")
 	if err != nil {
 		fmt.Printf("failed to mount /sys: %s\n", err.Error())
 		os.Exit(1)
 	}
+
 	if _, err = os.Stat("/dev"); err != nil {
 		err = os.Mkdir("/dev", 0755)
 		if err != nil {
@@ -52,18 +58,32 @@ func init() {
 		fmt.Printf("failed to mount /dev: %s\n", err.Error())
 		os.Exit(1)
 	}
-	err = os.Mkdir("/dev/pts", 0755)
-	if err != nil {
-		fmt.Printf("failed to create /dev/pts: %s\n", err.Error())
+
+	if _, err := os.Stat("/dev/pts"); err != nil {
+		err = os.Mkdir("/dev/pts", 0755)
+		if err != nil {
+			fmt.Printf("failed to create /dev/pts: %s\n", err.Error())
+		}
 	}
 	err = syscall.Mount("devpts", "/dev/pts", "devpts", 0, "gid=5,mode=620")
 	if err != nil {
 		fmt.Printf("failed to mount /dev/pts: %s\n", err.Error())
 		os.Exit(1)
 	}
-	err = os.Mkdir("/mnt", 0755)
-	if err != nil {
-		fmt.Printf("failed to create /mnt: %s\n", err.Error())
+
+	if _, err := os.Stat("/mnt"); err != nil {
+		err = os.Mkdir("/mnt", 0755)
+		if err != nil {
+			fmt.Printf("failed to create /mnt: %s\n", err.Error())
+		}
 	}
 
+	/*
+		cmd := exec.Command("/bin/busybox", "--help", "|", "/bin/busybox", "grep", "'Currently defined functions:'", "-A300", "|", "/bin/busybox", "grep", "-v", "'Currently defined functions:'", "|", "/bin/busybox", "tr", ",", "'\n'", "|", "/bin/busybox", "xargs", "-n1", "/bin/busybox", "ln", "-s", "busybox")
+		cmd.Dir = "/bin"
+		err = cmd.Run()
+		if err != nil {
+			fmt.Printf(err.Error())
+		}
+	*/
 }
